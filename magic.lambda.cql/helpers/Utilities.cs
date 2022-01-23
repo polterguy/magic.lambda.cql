a63482fb8401f02edfe0c69644ef837c3c783dc6
Thomas Hansen <thomas@gaiasoul.com>
2022-01-23 08:47:31 +0200

    Update Utilities.cs

--- magic.lambda.cql/helpers/Utilities.cs
+++ magic.lambda.cql/helpers/Utilities.cs
@@ -22,6 +22,25 @@ namespace magic.lambda.cql.helpers
         static readonly ConcurrentDictionary<string, Cluster> _clusters = new System.Collections.Concurrent.ConcurrentDictionary<string, Cluster>();
 
         /*
+         * Returns connection settings for Cluster and keyspace to caller given the  specified node.
+         */
+        internal static (string Cluster, string KeySpace) GetConnectionSettings(IConfiguration configuration, Node node)
+        {
+            var value = node.GetEx<string>();
+            if (value.StartsWith("[") && value.EndsWith("]"))
+            {
+                var splits = value.Substring(1, value.Length - 2).Split('|');
+                if (splits.Count() != 2)
+                    throw new HyperlambdaException($"I don't understand how to connect to a CQL database using '{value}'");
+                return (configuration[$"magic:cql:{splits[0]}:host"], splits[1]);
+            }
+            else
+            {
+                return ("generic", value);
+            }
+        }
+
+        /*
          * Creates a ScyllaDB session and returns to caller.
          */
         internal static ISession CreateSession(string cluster, string keyspace)
